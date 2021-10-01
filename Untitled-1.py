87c0aa912f49f85c0fba9c9ba709cbb3880ce4ee
Tejas Mehetre <85450083+tejasvm123@users.noreply.github.com>
2021-10-01 22:50:30 +0530

    main file

---
+++ Untitled-1.py
@@ -0,0 +1,20 @@
+[
+{
+    "id": "1",
+    "model": "Infrastructure as a Service(IaaS)",
+    "defination": "https://searchcloudcomputing.techtarget.com/definition/Infrastructure-as-a-Service-IaaS",
+    "examples": "https://www.jigsawacademy.com/blogs/cloud-computing/iaas-examples/"
+    },
+    {
+     "id": "2",
+     "model": "Platform as a Service(PaaS)",
+     "defination": "https://azure.microsoft.com/en-in/overview/what-is-paas/",
+     "examples": "https://www.webhostingsecretrevealed.net/blog/web-business-ideas/paas-examples/"
+    },
+    {
+     "id": "3",
+     "model": "Software as a Service(SaaS)",
+     "defination": "https://www.salesforce.com/in/saas/",
+     "examples": "https://joshfechter.com/software-service-examples/"
+    }
+]
